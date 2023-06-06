7a53061ef0fa8ff43d487b4a992053f9fb674ed4
Sergey Georgiev <sgrgv_art@mail.ru>
2023-06-06 13:48:50 +0300

    todo-list with typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,81 @@
+//packages
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+//types
+import { ITodo } from "../types/data";
+
+//components
+import { TodoList } from "./TodoList";
+
+const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+      setValue("");
+    }
+  };
+
+  const removeTodo = (id: number): void => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id: number): void => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      })
+    );
+  };
+
+  // изучить typescript на сайте code mu
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <input
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+    </div>
+  );
+};
+
+export { App };
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,24 @@
+//types
+import { ITodo } from "../types/data";
+
+interface ITodoItem extends ITodo {
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
+
+const TodoItem: React.FC<ITodoItem> = (props) => {
+  const { id, title, complete, removeTodo, toggleTodo } = props;
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={complete}
+        onChange={() => toggleTodo(id)}
+      />
+      {title}
+      <span onClick={() => removeTodo(id)}>&times;</span>
+    </div>
+  );
+};
+
+export { TodoItem };
